77afa11a01a3b4dd8a75b39d404b64762ae181a6
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 00:15:36 +0100

    create venv(import pandas) and begin ex00

--- ex00/load_csv.py
+++ ex00/load_csv.py
@@ -1,1 +1,27 @@
-# proto : def load(path: str) -> Dataset: (you have to adapt the type of return according to your library)
+import pandas as pd
+
+
+# panda : Biblio standard pour importer les csv
+
+def load(path: str) -> Dataset: # (you have to
+# adapt the type of return according to your library)
+    """
+    Docstring pour load
+    
+    :param path: Description
+    :type path: str
+    :return: Description
+    :rtype: Not defined yet bro
+    """
+    try:
+        # ici read_csv cree un "Dataframe" (un tab de données)
+        # Il faut lassigner a une variable pour taffer avec
+        tab_data = pd.read_csv(path)
+        # pour les dimensions on utilise shape comme module precedent
+        # (comme un attribut)
+        print(f"Dimensions du tableau : {tab_data.shape}")
+
+
+
+    except:
+
