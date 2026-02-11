71437cd233013d7210bba414df95499d2c78df29
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 01:55:29 +0100

    copie logique exo 1 + decla dune liste pour regler lafficahge de plusieurs location

---
+++ ex02/aff_pop.py
@@ -0,0 +1,40 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import MultipleLocator
+
+def main():
+    data_frame = load("life_expectancy_years.csv")
+    # on place lindex sur les pays
+    data_frame.set_index("country", inplace=True)
+    #on cible la france, la belgique et l allemagne mtn
+    pays_vises = ["France", "Belgium", "Germany"]
+    # on recup un tableau avec les pays vises
+    comparaison = data_frame.loc[pays_vises]
+    # en passant en int, laxe X devient une ligne numerique
+    comparaison.index = comparaison.index.astype(int)
+    #on prend ces donnees la.
+    plt.plot(comparaison.index, comparaison.values)
+
+    # on cree un titre
+    plt.title("Évolution de l'espérance de vie en France")
+    # On donne le label de laxe abscisse
+
+    plt.xlabel("Années")
+    # ordonnees.
+    plt.ylabel("Âge")
+    # On récupère l'objet "axe" du graphique actuel
+    ax = plt.gca()
+
+    # Un texte toutes les 50 années
+    ax.xaxis.set_major_locator(MultipleLocator(50))
+
+    #  Un petit trait (sans texte) toutes les 10 années
+    ax.xaxis.set_minor_locator(MultipleLocator(10))
+    # on ouvre une fenetre avec notre contenu.
+    plt.show()
+    return
+
+
+if __name__ == "__main__":
+  main()
---
+++ ex02/load_csv.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+# panda : Biblio standard pour importer les csv
+
+def load(path: str) -> pd.DataFrame | None:  # (you have to
+    # adapt the type of return according to your library)
+    """
+    Docstring pour load
+
+    :param path: Description
+    :type path: str
+    :return: Description
+    :rtype: Not defined yet bro
+    """
+    try:
+        # ici read_csv cree un "Dataframe" (un tab de données)
+        # Il faut lassigner a une variable pour taffer avec
+
+        if not path.endswith(".csv"):
+            raise AssertionError("Wrong Extension Format")
+        tab_data = pd.read_csv(path)
+        # pour les dimensions on utilise shape comme module precedent
+        # (comme un attribut)
+        print(f"Loadding dataset of dimensions {tab_data.shape}")
+
+        return tab_data
+
+    except FileNotFoundError:
+        print("Error: The file was not found.")
+        return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
