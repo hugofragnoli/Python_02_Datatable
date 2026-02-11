086caa226309c391011856c3e16c588f569bc943
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 15:55:16 +0100

    begin ex03

---
+++ ex03/projection_life.py
@@ -0,0 +1,14 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import MultipleLocator
+
+def util_func():
+
+
+
+    return 
+
+def main():
+    data_life_exp = load("life_expectancy_years.csv")
+    data_income_per_pp = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
