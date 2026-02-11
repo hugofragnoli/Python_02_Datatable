9cd900897fbbbd2383e9aa33f1397f4a7725aeb2
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 01:48:18 +0100

    corr display years grace a Multiple locator et passage de lindex years en int

--- ex01/all_life.py
+++ ex01/all_life.py
@@ -1,18 +1,35 @@
 from load_csv import load
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.ticker import MultipleLocator
 
 def main():
     data_frame = load("life_expectancy_years.csv")
+    # on place lindex sur les pays
     data_frame.set_index("country", inplace=True)
-    france = data_frame.loc["India"]
-
+    #on cible la france.
+    france = data_frame.loc["France"]
+    # en passant en int, laxe X devient une ligne numerique
+    france.index = france.index.astype(int)
+    #on prend ces donnees la.
     plt.plot(france.index, france.values)
 
+    # on cree un titre
     plt.title("Évolution de l'espérance de vie en France")
-    plt.xlabel("Années")
-    plt.ylabel("Âge")
+    # On donne le label de laxe abscisse
 
+    plt.xlabel("Années")
+    # ordonnees.
+    plt.ylabel("Âge")
+    # On récupère l'objet "axe" du graphique actuel
+    ax = plt.gca()
+
+    # Un texte toutes les 50 années
+    ax.xaxis.set_major_locator(MultipleLocator(50))
+
+    #  Un petit trait (sans texte) toutes les 10 années
+    ax.xaxis.set_minor_locator(MultipleLocator(10))
+    # on ouvre une fenetre avec notre contenu.
     plt.show()
     return
 
