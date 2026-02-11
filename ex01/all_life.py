8389741811ae6abda374e1808455cbf40f1ce008
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 01:15:07 +0100

    debut ex01 import matplotlib pour graph et load du csv

---
+++ ex01/all_life.py
@@ -0,0 +1,23 @@
+from load_csv import load
+import matplotlib as plt
+import pandas as pd
+
+def main(data_frame: pd.DataFrame) -> any:
+    data_frame = load("life_expectancy_years.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+  main()
