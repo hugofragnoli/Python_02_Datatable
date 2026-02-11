f61857be739075e94c3bff0aad1c516242a8d9f5
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 01:26:54 +0100

    supp param fct et import .pyplot pour le graphique

--- ex01/all_life.py
+++ ex01/all_life.py
@@ -1,14 +1,20 @@
 from load_csv import load
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
-def main(data_frame: pd.DataFrame) -> any:
+def main():
     data_frame = load("life_expectancy_years.csv")
     data_frame.set_index("country", inplace=True)
     france = data_frame.loc["France"]
-    
 
+    plt.plot(france.index, france.values)
 
+    plt.title("Évolution de l'espérance de vie en France")
+    plt.xlabel("Années")
+    plt.ylabel("Âge")
+
+    plt.show()
+    return
 
 
 if __name__ == "__main__":
