cb1c63aec1799924f3144a42e061e83cbde72572
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 16:37:38 +0100

    merge data 1900 des 2  tableaux

--- ex03/projection_life.py
+++ ex03/projection_life.py
@@ -4,16 +4,21 @@ import pandas as pd
 from matplotlib.ticker import MultipleLocator
 
 def merge_func(df_life: pd.DataFrame, df_income: pd.DataFrame) -> pd.DataFrame:
-    
+    # On extrait uniquement la colonne country et l'année 1900
+    # On utilise .copy() pour éviter les warnings de Pandas
+    life_1900 = df_life[["country", "1900"]].copy()
+    income_1900 = df_income[["country", "1900"]].copy()
+    # On fusionne sur la colonne 'country'
+    # suffixes permet de différencier les deux colonnes '1900'
+    merged = pd.merge(life_1900, income_1900, on="country", suffixes=("_life", "_income"))
 
 
-
-    return merged_data
+    return merged
 
 def main():
     data_life_exp = load("life_expectancy_years.csv")
     data_income_per_pp = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
-    merge_data = merge_func()
+    merge_data = merge_func(data_life_exp, data_income_per_pp)
 
 
 if __name__ == "__main__":
