a63cc25d7e02aad6746b47b6ad2089f6af4fc8da
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 16:23:42 +0100

    debut merge_func ex03

--- ex03/projection_life.py
+++ ex03/projection_life.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.ticker import MultipleLocator
 
-def merge_func(arrays: pd.DataFrame) -> pd.DataFrame:
+def merge_func(df_life: pd.DataFrame, df_income: pd.DataFrame) -> pd.DataFrame:
+    
 
 
 
-
-    return 
+    return merged_data
 
 def main():
     data_life_exp = load("life_expectancy_years.csv")
     data_income_per_pp = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
-    merge_func()
+    merge_data = merge_func()
 
 
 if __name__ == "__main__":
