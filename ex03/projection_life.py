0f38142f9e79e75aa7ce55977600d2e8ad37fec4
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 18:00:42 +0100

    +flake8 ex03

--- ex03/projection_life.py
+++ ex03/projection_life.py
@@ -1,7 +1,7 @@
 from load_csv import load
 import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.ticker import MultipleLocator
+
 
 def merge_func(df_life: pd.DataFrame, df_income: pd.DataFrame) -> pd.DataFrame:
     # On extrait uniquement la colonne country et l'année 1900
@@ -10,14 +10,15 @@ def merge_func(df_life: pd.DataFrame, df_income: pd.DataFrame) -> pd.DataFrame:
     income_1900 = df_income[["country", "1900"]].copy()
     # On fusionne sur la colonne 'country'
     # suffixes permet de différencier les deux colonnes '1900'
-    merged = pd.merge(life_1900, income_1900, on="country", suffixes=("_life", "_income"))
-
-
+    merged = pd.merge(life_1900, income_1900, on="country",
+                      suffixes=("_life", "_income"))
     return merged
+
 
 def main():
     data_life_exp = load("life_expectancy_years.csv")
-    data_income_per_pp = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+    data_income_per_pp = load("income_per_person_gdppercapita_"
+                              "ppp_inflation_adjusted.csv")
     merge_data = merge_func(data_life_exp, data_income_per_pp)
     # debug pour afficher types de data : print(merge_data.dtypes)
     plt.scatter(merge_data["1900_income"], merge_data["1900_life"])
@@ -30,7 +31,8 @@ def main():
 
     # on dit de mettre que ces indices la sur laxe X
     plt.xticks([300, 1000, 10000], ['300', '1k', '10k'])
-    plt.show()  
+    plt.show()
+
 
 if __name__ == "__main__":
-  main()
+    main()
