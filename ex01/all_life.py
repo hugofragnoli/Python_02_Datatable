708cb7376f02c45a01e4c0c9fa3f264a8bd2568c
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-11 01:23:23 +0100

    add index countries + isolation de la donnée

--- ex01/all_life.py
+++ ex01/all_life.py
@@ -4,17 +4,9 @@ import pandas as pd
 
 def main(data_frame: pd.DataFrame) -> any:
     data_frame = load("life_expectancy_years.csv")
-
-
-
-
-
-
-
-
-
-
-
+    data_frame.set_index("country", inplace=True)
+    france = data_frame.loc["France"]
+    
 
 
 
